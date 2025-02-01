57eeca269e4979f251f9b8d46865f4070e566b4c
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2025-01-31 19:50:09 -0600

    Create lab1.h

---
+++ lab1/lab1.h
@@ -0,0 +1,7 @@
+#ifndef _LAB1_H
+#define _LAB1_H
+// Function Prototypes
+int getMedian(int array[], int size, int *result);
+int sumNegative(int array[], int size, int *result);
+int sumEven(int array[], int size, int *result);
+#endif
