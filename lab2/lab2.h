6c1111d352694588158b212dd8ae969b43bb56d1
J-eyJ <60826745+J-D-J123@users.noreply.github.com>
2025-02-11 10:50:46 -0600

    Create lab2.h

---
+++ lab2/lab2.h
@@ -0,0 +1,8 @@
+#ifndef _LAB2_H
+#define _LAB2_H
+// Prototypes
+int makeArray(int **array, int size);
+void initArray(int *array, int size);
+int multiplyOdd(int *array, int size, int multiplicand);
+void freeArray(int **array);
+#endif
